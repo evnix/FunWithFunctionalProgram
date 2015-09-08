8bdca36faa35bd1d4ba7fcda051228fa19ab4f7d
evnix <evnix.com@gmail.com>
2015-09-08 18:26:35 +0530

    added files2

--- fhandle.erl
+++ fhandle.erl
@@ -9,7 +9,33 @@ getNumbers(Line)->
 	binary:split(Line, [<<" ">>], [global]).
 
 
-loadArray(Array,FileName)->
+
+loadNumbers(Array,Numbers,Index,CurrSize,Size) ->
+	
+	case CurrSize < Size of
+
+	true -> 
+		[Num|RNumbers] = Numbers,
+		two_dim:set(Index,CurrSize,Num,Array),
+
+
+loadLines(Array,Lines,CurrSize,Size) ->
+	
+	case CurrSize < Size of
+
+	true -> 
+		[Line|RLines]=Lines,
+		Numbers = getNumbers(Line),
+		loadNumbers(Array,Numbers,CurrSize,0,Size),
+		loadLines(Array,RLines,CurrSize+1,Size);
+
+	false -> 1=1
+
+	end. 
+
+
+loadArray(Array,FileName,Size)->
 	TLines = fhanlde:loadfile(FileName),
 	[_|Lines] = TLines,
-	
+	loadLines(Array,Lines,0,Size),
+
